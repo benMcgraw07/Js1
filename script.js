5e7dd61b0ddb11d13e33819525d3ffd5061c9732
benMcgraw07 <benrm007@gmail.com>
2026-01-22 15:24:13 +0000

    decrement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var myVar = 87;
 
 //only change code below this line
-myVar = myVar + 1;
+myVar = myVar - 1;
 
-//quicker way to increment a number 
-myVar++; // increment 
+//quicker way to decrement  a number 
+myVar--; // decrement
