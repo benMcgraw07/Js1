b86df5a1134816fcd5e1d43a20b79256d4c19a1b
benMcgraw07 <benrm007@gmail.com>
2026-01-22 15:12:30 +0000

    uninitialized variables examples

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // initialize these 3 variables
-var a;
-var b;
-var c;
+var a = 5;
+var b = 10;
+var c = "I am a string, ";
 
 // do not change code below this line
 
 a = a + 1;
 b = b + 5;
-c = c + "String";
+c = c + "String!";
+
+console.log(a);
+console.log(b);
+console.log(c);
